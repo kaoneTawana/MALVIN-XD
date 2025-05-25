57c9af06aa3cee36c261f382e40d0fd6385cba0a
kaoneTawana <kaonetawana445@gmail.com>
2025-05-26 00:05:34 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ MODE: process.env.MODE || "public",
 
 
         AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-// make true or false status auto seen
+// make true  false status auto seen
 
         AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
